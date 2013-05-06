af6dc7a52fbef99e6776136f3eaed667b0f391f6
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2013-05-06 17:11:32 +1200

    Detecting page and section breaks.

--- mailmerge-splitter/Form1.Designer.cs
+++ mailmerge-splitter/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(45, 41);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(190, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(241, 38);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(31, 23);
+            this.btnOpenFileDialog.TabIndex = 1;
+            this.btnOpenFileDialog.Text = "...";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File:";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(197, 227);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnOpenFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
     }
 }
 
--- mailmerge-splitter/Form1.cs
+++ mailmerge-splitter/Form1.cs
@@ -12,9 +12,78 @@ namespace mailmerge_splitter
 {
     public partial class Form1 : Form
     {
+        private log4net.ILog _log = log4net.LogManager.GetLogger(typeof(Form1));
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = txtFilePath.Text;
+                _log.DebugFormat("Opening document {0}...", fileName);
+
+                using (DocumentFormat.OpenXml.Packaging.WordprocessingDocument document = DocumentFormat.OpenXml.Packaging.WordprocessingDocument.Open(fileName, true))
+                {
+                    DocumentFormat.OpenXml.Wordprocessing.Body body = document.MainDocumentPart.Document.Body;
+
+                    ExtractPages(document);
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+            }
+        }
+
+        public void ExtractPages(DocumentFormat.OpenXml.Packaging.WordprocessingDocument document)
+        {
+            IEnumerable<DocumentFormat.OpenXml.Wordprocessing.Paragraph> topP = document.MainDocumentPart.Document.Body.Elements<DocumentFormat.OpenXml.Wordprocessing.Paragraph>();
+            foreach (DocumentFormat.OpenXml.Wordprocessing.Paragraph p in topP)
+            {
+                _log.DebugFormat("p: {0}", p.InnerText);
+                if (ContainSectionBreak(p))
+                {
+                    _log.Debug("============= Section Break =============");
+                }
+                if (ContainPageBreak(p))
+                {
+                    _log.Debug("============= Page Break =============");
+                }
+            }
+        }
+        public bool ContainPageBreak(DocumentFormat.OpenXml.Wordprocessing.Paragraph p)
+        {
+            return p.Elements<DocumentFormat.OpenXml.Wordprocessing.Run>().FirstOrDefault(r => r.Elements<DocumentFormat.OpenXml.Wordprocessing.Break>().FirstOrDefault(b => b.Type == DocumentFormat.OpenXml.Wordprocessing.BreakValues.Page) != null) != null;
+        }
+        public bool ContainLastRenderedPageBreak(DocumentFormat.OpenXml.Wordprocessing.Paragraph p)
+        {
+            return p.Elements<DocumentFormat.OpenXml.Wordprocessing.Run>().FirstOrDefault(r => r.Elements<DocumentFormat.OpenXml.Wordprocessing.LastRenderedPageBreak>().FirstOrDefault() != null) != null;
+        }
+        public bool ContainSectionBreak(DocumentFormat.OpenXml.Wordprocessing.Paragraph p)
+        {
+            if (p.ParagraphProperties != null && p.ParagraphProperties.SectionProperties != null)
+            {
+                if (p.ParagraphProperties.SectionProperties.LocalName == "sectPr")
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Word Documents (*.doc/*.docx)|*.doc;*.docx|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
         }
     }
 }
